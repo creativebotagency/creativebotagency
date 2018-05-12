ccb4e887ef0e8da821014481f43453f480bf3075
Hassan Shah <hassans.1994@gmail.com>
2018-05-12 15:15:04 +0100

    style: using ionicons

--- src/app/sections/services/services.component.ts
+++ src/app/sections/services/services.component.ts
@@ -20,32 +20,32 @@ export class ServicesComponent implements OnInit {
     this.services.push(new Service(
       'Mobile App Development',
       'Cross platform mobile app development for iOS, Android and Windows',
-      'fa-mobile'
+      'ion-ios-phone-portrait'
     ));
     this.services.push(new Service(
       'Website Development',
       'Development of beautiful, mobile-friendly websites of any scale',
-      'fa-desktop'
+      'ion-ios-desktop'
     ));
     this.services.push(new Service(
       'UI & UX Design',
       'We can create the user interface and user experience for your product with an exceptinal attention to detail',
-      'fa-paint-brush'
+      'ion-ios-color-filter'
     ));
     this.services.push(new Service(
       'E-commerce',
       'We use Woocommerce to create feature-rich e-commerce websites',
-      'fa-shopping-cart'
+      'ion-ios-cart'
     ));
     this.services.push(new Service(
       'Wordpress Development',
       'We can create a content management system for your website using Wordpress',
-      'fa-wordpress'
+      'ion-logo-wordpress'
     ));
     this.services.push(new Service(
       'Support & Maintenance',
       'We can offer you any support that you require and can look after your app or website',
-      'fa-wrench'
+      'ion-ios-hammer'
     ));
   }
 
